76f11777dbe39df8906a8913dce74b5899c7d5ae
Kok-spec <kokoa.kawaraya.yw@ps.hitachi-solutions.com>
2025-10-21 10:02:55 +0900

    008

--- src/components/PanoramaViewer.tsx
+++ src/components/PanoramaViewer.tsx
@@ -2,19 +2,27 @@ import { Canvas, useLoader } from '@react-three/fiber';
 import { TextureLoader } from 'three';
 import * as THREE from 'three';
 import { OrbitControls } from '@react-three/drei';
-import panoramaImage from './panorama-image.jpg'; // ←同じフォルダなので ./ でOK！
+import panoramaImage from './panorama-image.jpg';
 
 const PanoramaViewer = () => {
   const texture = useLoader(TextureLoader, panoramaImage);
 
   return (
-    <Canvas className="w-full h-full">
-      <OrbitControls enableZoom={false} />
-      <mesh>
-        <sphereGeometry args={[500, 60, 40]} />
-        <meshBasicMaterial map={texture} side={THREE.BackSide} />
-      </mesh>
-    </Canvas>
+    <div className="w-full h-full cursor-grab active:cursor-grabbing">
+      <Canvas className="w-full h-full">
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          enableDamping={true}   
+          dampingFactor={0.05}   
+          rotateSpeed={0.8}      
+        />
+        <mesh>
+          <sphereGeometry args={[500, 60, 40]} />
+          <meshBasicMaterial map={texture} side={THREE.BackSide} />
+        </mesh>
+      </Canvas>
+    </div>
   );
 };
 
